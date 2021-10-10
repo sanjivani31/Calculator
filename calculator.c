3f30114684732a35bf2e84ee4fab3476bdb2ae40
sanjivani31 <91777885+sanjivani31@users.noreply.github.com>
2021-10-10 11:52:56 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+   int n1, n2, ans;
+   char op;
+   
+   
+   printf("Enter an operator:\n");
+   scanf("%c",&op);
+   
+   printf("enter values for n1 and n2:");
+   scanf("%d%d",&n1,&n2);
+   
+   switch(op)
+   {
+      case '+':
+         ans = n1 + n2;
+         printf("The sum is: %d\n",ans);
+         break;
+      
+      case '-':
+         ans = n1 - n2;
+         printf("The substration is: %d\n",ans);
+         break;
+         
+      case '*':
+         ans = n1 * n2;
+         printf("The multipkication is: %d\n",ans);
+         break;
+         
+      case '/':
+         ans = n1 / n2;
+         printf("The division is: %d\n",ans);
+         break;
+      
+      case '%':
+         ans = n1 % n2;
+         printf("The remainder is: %d\n",ans);
+         break;
+         
+      default: printf("Enter a valid operator.\n");
+      
+   }
+   
+   printf("Thank you!");
+   
+}
+
