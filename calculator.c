c75464562576ad2f5a2e55b4234eedee5eee19bf
sanjivani31 <91777885+sanjivani31@users.noreply.github.com>
2021-10-16 21:25:23 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -2,42 +2,40 @@
 
 int main()
 {
-   int n1, n2, ans;
+   int n1, n2;
    char op;
    
    
    printf("Enter an operator:\n");
    scanf("%c",&op);
    
-   printf("enter values for n1 and n2:");
+   printf("Enter values for n1 and n2:");
    scanf("%d%d",&n1,&n2);
    
    // To check which operation have to perform
    switch(op)
    {
       case '+':
-         ans = n1 + n2;
-         printf("The sum is: %d\n",ans);
+       
+         printf("The sum is: %d\n",n1+n2);
          break;
       
       case '-':
-         ans = n1 - n2;
-         printf("The substration is: %d\n",ans);
+         
+         printf("The substration is: %d\n",n1-n2);
          break;
          
       case '*':
-         ans = n1 * n2;
-         printf("The multipkication is: %d\n",ans);
+         
+         printf("The multipkication is: %d\n",n1*n2);
          break;
          
       case '/':
-         ans = n1 / n2;
-         printf("The division is: %d\n",ans);
+         
+         printf("The division is: %d\n",n1/n2);
          break;
-      
-      case '%':
-         ans = n1 % n2;
-         printf("The remainder is: %d\n",ans);
+      //ans = n1 % n2;
+         printf("The remainder is: %d\n",n1%n2);
          break;
          
       default: printf("Enter a valid operator.\n");
